ade28ee52ba3f9f79f9978d0ac8304939dbf0847
Kgomotso <kgomotsolouisaseema@gmail.com>
2023-07-13 15:51:51 +0200

    COURSES Appear on UI

--- script.js
+++ script.js
@@ -1,97 +1,176 @@
 const faculties = [
-    { name: 'IT', courses: [
-        { courseName: 'Computer Engineering', requirements: {
-            aps: 30, subjects: [ 'Maths', 'Science' ]
-        }},
-        { courseName: 'Information Technology: Software Development', requirements: {
-            aps: 26, subjects: [ 'Maths', 'Science' ]
-        }},
-        { courseName: 'Information Technology: Web and Application Development', requirements: {
-            aps: 20, subjects: [ 'Maths', 'Science' ]
-        }}
-    ]},
-    { name: 'Science', courses: [
-        { courseName: 'Biotechnology', requirements: {
-            aps: 30, subjects: [ 'Maths', 'Science', 'Life Sciences' ]
-        }},
-        { courseName: 'Chemistry', requirements: {
-            aps: 26, subjects: [ 'Maths', 'Science' ]
-        }},
-        { courseName: 'Industry Physics', requirements: {
-            aps: 20, subjects: [ 'Maths', 'Science' ]
-        }}
-    ]},
-    { name: 'Art & Design', courses: [
-        { courseName: 'Design', requirements: {
-            aps: 30, subjects: [ 'Maths', 'Science' ]
-        }},
-        { courseName: 'Performing Arts', requirements: {
-            aps: 26, subjects: [ 'Maths', 'Science' ]
-        }},
-        { courseName: 'Fine Art', requirements: {
-            aps: 20, subjects: [ 'Maths', 'Science' ]
-        }}
-    ]},
-    { name: 'Humanities', courses: [
-        { courseName: 'Education', requirements: {
-            aps: 30, subjects: [ 'Maths', 'Science', 'English' ]
-        }},
-        { courseName: 'Journalism', requirements: {
-            aps: 26, subjects: [ 'Maths', 'Science', 'English' ]
-        }}
-    ]}
-] 
+  {
+    name: "IT",
+    courses: [
+      {
+        courseName: "Computer Engineering",
+        requirements: {
+          aps: 30,
+          subjects: ["Maths", "Science"],
+        },
+      },
+      {
+        courseName: "Information Technology: Software Development",
+        requirements: {
+          aps: 26,
+          subjects: ["Maths", "Science"],
+        },
+      },
+      {
+        courseName: "Information Technology: Web and Application Development",
+        requirements: {
+          aps: 20,
+          subjects: ["Maths", "Science"],
+        },
+      },
+    ],
+  },
+  {
+    name: "Science",
+    courses: [
+      {
+        courseName: "Biotechnology",
+        requirements: {
+          aps: 30,
+          subjects: ["Maths", "Science", "Life Sciences"],
+        },
+      },
+      {
+        courseName: "Chemistry",
+        requirements: {
+          aps: 26,
+          subjects: ["Maths", "Science"],
+        },
+      },
+      {
+        courseName: "Industry Physics",
+        requirements: {
+          aps: 20,
+          subjects: ["Maths", "Science"],
+        },
+      },
+    ],
+  },
+  {
+    name: "Art & Design",
+    courses: [
+      {
+        courseName: "Design",
+        requirements: {
+          aps: 30,
+          subjects: ["Maths", "Science"],
+        },
+      },
+      {
+        courseName: "Performing Arts",
+        requirements: {
+          aps: 26,
+          subjects: ["Maths", "Science"],
+        },
+      },
+      {
+        courseName: "Fine Art",
+        requirements: {
+          aps: 20,
+          subjects: ["Maths", "Science"],
+        },
+      },
+    ],
+  },
+  {
+    name: "Humanities",
+    courses: [
+      {
+        courseName: "Education",
+        requirements: {
+          aps: 30,
+          subjects: ["Maths", "Science", "English"],
+        },
+      },
+      {
+        courseName: "Journalism",
+        requirements: {
+          aps: 26,
+          subjects: ["Maths", "Science", "English"],
+        },
+      },
+    ],
+  },
+];
 
+// kgomotso louisa
+let course = [];
+let studentsCourses = []; //this array will collect the initial course that user qualifies for when btn1 is clicked
 
-// kgomotso louisa 
-courses = [];
-studentsCourses = [];
+let btn1Enabled = false; //Initialized check Course button to false at first
+let selectedScoreRange = " "; //holds the selected range of APS scores from user
 
-btn1Enabled = false;
+let subjectsSelected = []; // this array holds the selected subjects from the user
 
 const apsScoreRange = document.getElementById("apsSelect");
-apsScoreRange.addEventListener("change",onApsScoreSelected);
-const valueScore = apsScoreRange.value;
+apsScoreRange.addEventListener("change", onApsScoreSelected); //event listener for apsScoreSelected function
 
-//3. Event Handlers for the APS SCORE and Subjects functions
-//APS score function
-
-function onApsScoreSelected(event){
-    const currentApsScore = event.target.value;
-    let apsScore = faculties[1].courses[0].requirements.aps  //this line only iterates by line and not the whole faculties array
-    console.log(apsScore)
-//this code must iterate through the length of the faculties array to compare the aps score with the course
-    for(let i= 0 ; i <  faculties.courses.requirements.length  ; i ++){
-        if(currentApsScore >= faculties[i].aps ){   // apsScore is defined in 64 and should ideally loop throught aps to check if student qualifies or doesnt . currentApsScor = users input range 
-            console.log("User Qualifies")
-        }else{
-            console.log("User doesnt qualifies for this course")
-        }
-        console.log(faculties.courses[i].requirements.aps.length )
-    }}
-    
-    
-
-    
-//Subjects Selected Function 
-function onSubjectChecked(subject){
-    subjectsSelected = event.target.value
-    //lost a bit here , come back to this one .
-    // subjectCheked(subject)
-    enableButtonIfValid()
-}
-
-// Button Enabled when user selects aps scroe range and atleast 1 subject 
- function enableButtonIfValid(){
-    if(selectedScoreRange && subjectsSelected.length > 0){
-        btn1Enabled = true
-    }else{
-        btn1Enabled = false  // check line 4 
+// APS score function to Iterate through the faculties array ,loop through and collect APS score
+function onApsScoreSelected(event) {
+  const currentApsScore = event.target.value;
+  selectedScoreRange = currentApsScore; // currentApsScore is now the value of user and
+  console.log(selectedScoreRange);
+  for (let i = 0; i < faculties.length; i++) {
+    // this for loop iterates through the faculties array first
+    const faculty = faculties[i]; //  c
+    for (let j = 0; j < faculty.courses.length; j++) {
+      const course = faculty.courses[j];
+      if (currentApsScore >= course.requirements.aps) {
+        console.log("Student Qualifies for course: ", course.name);
+        // Add the course to studentsCourses array
+        studentsCourses.push(course);
+      } else {
+        console.log("User doesn't qualify for course: ", course.name);
+      }
     }
+  }
+
+  enableButtonIfValid();
 }
 
-// 5 .checkCourses Function called when the submit button is clicked only when user has selected range and subject: 
-function checkCourses(courses){
-    studentsCourses = []
-// add muuuchh logic here 
+// Subjects Selected Function
+function onSubjectChecked(event) {
+  const selectedSubject = event.target.value;
+  subjectsSelected.push(selectedSubject);
+  enableButtonIfValid();
 }
+
+// Button Enabled when user selects APS score range and at least 1 subject . i should add functionaliity to enabled the button.
+function enableButtonIfValid() {
+  if (selectedScoreRange && subjectsSelected.length > 0) {
+    btn1Enabled = true;
+  } else {
+    btn1Enabled = false;
+  }
+}
+
+// checkCourses Function called when the submit button is clicked only when user has selected range and subject
+function checkCourses() {
+  if (btn1Enabled) {
+//  logic for checking courses and performing necessary actions
+    console.log("Checking courses...");
+    console.log("Selected Subjects: ", subjectsSelected);
+    console.log("Student's Courses: ", studentsCourses);
+// 3:45 PM 13/7/JULY 160-164 DISPLAY THE COURSES STUDENTS QUALIFIES FOR 
+    let tempData = "<ul>";
+    for (let s = 0; s < studentsCourses.length; s++) {
+      tempData += "<li>" + studentsCourses[s].courseName + "</li>";
+    }
+    tempData += "</ul>";
+
+    document.getElementById("studentCanApply").innerHTML = tempData;
+  } else {
+    alert("Please select APS score range and at least 1 subject.");
+  }
+}
+// trying very hard to get the student course that they qualify for to display on UI.
+// let dropdown = document.getElementsByClassName("dropdownCourses");
+// dropdown.innerHTML = JSON.stringify(studentsCourses);
+
+//this function displays the course on the UI after the checkCourses function has excuted
+function displayCourse() {}
