1708cd5eb5bf22c19b64907f7b531873f78723e8
Kgomotso <kgomotsolouisaseema@gmail.com>
2023-07-14 16:01:02 +0200

    final Submit

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ const faculties = [
 // kgomotso louisa
 let course = [];
 let studentsCourses = []; //this array will collect the initial course that user qualifies for when btn1 is clicked
+// let qualifyingCourses = []; 
 
 let btn1Enabled = false; //Initialized check Course button to false at first
 let selectedScoreRange = " "; //holds the selected range of APS scores from user
@@ -157,13 +158,13 @@ function checkCourses() {
     console.log("Selected Subjects: ", subjectsSelected);
     console.log("Student's Courses: ", studentsCourses);
 // 3:45 PM 13/7/JULY 160-164 DISPLAY THE COURSES STUDENTS QUALIFIES FOR 
-    let tempData = "<ul>";
+    let tempData = "<ol>";
     for (let s = 0; s < studentsCourses.length; s++) {
       tempData += "<li>" + studentsCourses[s].courseName + "</li>";
     }
-    tempData += "</ul>";
+    tempData += "</ol>"
 
-    document.getElementById("studentCanApply").innerHTML = tempData;
+    document.getElementById("studentCanApply").innerHTML = ` YOU QUALIFY FOR THE FOLLOWING COURSES :  ${tempData}`;
   } else {
     alert("Please select APS score range and at least 1 subject.");
   }
@@ -173,4 +174,58 @@ function checkCourses() {
 // dropdown.innerHTML = JSON.stringify(studentsCourses);
 
 //this function displays the course on the UI after the checkCourses function has excuted
-function displayCourse() {}
+function submitApplication(){
+  let name = document.getElementById("name").value;
+  let id = document.getElementById("id").value;
+  let age = document.getElementById("age").value;
+  let email = document.getElementById("email").value;
+  // let selectedCourse = document.getElementById("dropdownCourses").value;
+
+  let applicationData = [name,id,age,email]; //,selectedCourse
+
+  console.log("Application submitted:");
+  console.log("Name: " + applicationData[0]);
+  console.log("ID: " + applicationData[1]);
+  console.log("Age: " + applicationData[2]);
+  console.log("Email: " + applicationData[3]);
+  // console.log("Course: " + applicationData[4]);
+  alert("Your application has been submitted , Thank you !");
+
+   // Optionally,reset the form fields after submission.
+   document.getElementById("application-form").reset();
+
+}
+
+function getCourse(){
+  selectCourse =document.getElementById("dropdownCourses");
+  console.log(selectCourse);
+  output= selectCourse.options[selectCourse.selectedIndex].value; //this gets the options from the dropdown list of course
+
+  document.querySelector('.output').textContent = output;
+      
+}
+// this function was supposed to filter the users qualifying courses and then can they apply, it has not worked  yet as of 3:31pm friday 
+
+// function limitCourseApply() {
+//   let selectEle = document.getElementById("dropdownCourses");
+//   for (let a = 0; a < selectEle.options.length; a++) {
+//     let option = selectEle.options[a];
+//     if (qualifyingCourses.some(course => course.courseName === option.value)) {
+//       option.disabled = false;
+//     } else {
+//       option.disabled = true;
+//     }
+//   }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   onApsScoreSelected({ target: apsScoreRange }); // Pass a mock event to trigger the function
+//   const subjectCheckboxes = document.querySelectorAll('.subjects input[type="checkbox"]');
+//   subjectCheckboxes.forEach(checkbox => {
+//     checkbox.addEventListener('change', function(event) {
+//       onSubjectChecked(event);
+//     });
+//   });
+
+//   limitCourseApply();
+// });
+
